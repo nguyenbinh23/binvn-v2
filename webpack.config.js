e210a94c61c60471d4e29133da58e9efef3ac5f7
Nguyễn Bình <ntuancold@gmail.com>
2020-09-02 21:04:39 +0700

    new src

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+
+
+
+/***************
+ * Lấy đường dẫn folder của file 
+    path.dirname('/public/demo/freetuts/logo.png'); 
+    Kết quả: public/demo/freetuts
+    
+***************/
+
+module.export = {
+
+}
