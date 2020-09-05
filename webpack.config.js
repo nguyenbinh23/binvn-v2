98e477de09c49f63482a2259205e148acf406014
Nguyễn Bình <ntuancold@gmail.com>
2020-09-05 19:14:52 +0700

    new change

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,24 @@
-
+/*
+    Cài đặt webpack
+    npm init -y <=== cài đặt node_modules
+    npm install -d webpack webpack-cli <=== cài đặt webpack
+    npm i html-webpack-plugin
+*/
 
 
 /***************
  * Lấy đường dẫn folder của file 
     path.dirname('/public/demo/freetuts/logo.png'); 
     Kết quả: public/demo/freetuts
-    
+
 ***************/
 
-module.export = {
-
+module.exports = {
+    mode: 'development',
+    entry: {
+        filename: 'index.js'
+    },
+    output: {
+        filename: '[name].js'
+    }
 }
